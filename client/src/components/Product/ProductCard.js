52ff6ea3aef0bc373c4e8c91232573b95f57ad1e
alinabezux <alinabezux@gmail.com>
2025-08-12 19:12:30 +0300

    tag card

--- client/src/components/Product/ProductCard.js
+++ client/src/components/Product/ProductCard.js
@@ -78,12 +78,13 @@ const ProductCard = memo(({ product }) => {
     return (
         <>
             <Card className="product-card" sx={{ '&:hover': { boxShadow: 'md', borderColor: 'neutral.outlinedHoverBorder' } }}
+                data-product-name={product.name}
+                data-product-url={`/product/${toUrlFriendly(product.name)}`}
                 onClick={() => handleShowDetails(product)}>
                 <Link className='link'
                     to={`/product/${(toUrlFriendly(product.name))}`}
                     key={product._id}
-                    data-product-name={product.name}
-                    data-product-url={`/product/${toUrlFriendly(product.name)}`}>
+                >
                     <AspectRatio ratio="1">
                         <CardOverflow>
                             {product.discount > 0 ?
